bf94afcdb4d773f3f9bc5248cbfad951e56c6847
clough42 <clough42@gmail.com>
2024-09-28 22:35:43 -0600

    Change CPP style so this doesn't look like it was coded by a blind chimpanzee on crack.

--- Firmware/ClearCore/SurfaceGrinder/main.cpp
+++ Firmware/ClearCore/SurfaceGrinder/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 
 #include <ClearCore.h>
 #include <DigitalInOut.h>
@@ -12,62 +13,58 @@
 int32_t velocity = 100 RPM;
 int32_t acceleration = 1000 RPM; // per second
 
-DigitalInOut *EStop = &ConnectorIO1;
-DigitalInOut *LeftLimit = &ConnectorIO2;
-DigitalInOut *RightLimit = &ConnectorIO3;
+auto& EStop = ConnectorIO1;
+auto& LeftLimit = ConnectorIO2;
+auto& RightLimit = ConnectorIO3;
 
-MotorDriver *XMotor = &ConnectorM0;
+auto& XMotor = ConnectorM0;
 
-typedef enum {
-	MOVING_RIGHT,
-	MOVING_LEFT
-	} MoveDirection;
-
+enum class MoveDirection { MOVING_RIGHT, MOVING_LEFT };
 
 int main(void) {
-	EStop->Mode(Connector::INPUT_DIGITAL);
-	LeftLimit->Mode(Connector::INPUT_DIGITAL);
-	RightLimit->Mode(Connector::INPUT_DIGITAL);
+	EStop.Mode(Connector::INPUT_DIGITAL);
+	LeftLimit.Mode(Connector::INPUT_DIGITAL);
+	RightLimit.Mode(Connector::INPUT_DIGITAL);
 	
 	MotorMgr.MotorInputClocking(MotorManager::CLOCK_RATE_NORMAL);
 	MotorMgr.MotorModeSet(MotorManager::MOTOR_M0M1, Connector::CPM_MODE_STEP_AND_DIR);
 	
-	XMotor->EnableRequest(true);
+	XMotor.EnableRequest(true);
 	
-	XMotor->AccelMax(acceleration);
-	XMotor->MoveStopDecel(0);
+	XMotor.AccelMax(acceleration);
+	XMotor.MoveStopDecel(0);
 	
-	MoveDirection currentMove = MOVING_RIGHT;
+	MoveDirection currentMove = MoveDirection::MOVING_RIGHT;
 	
-    while (1)
+    while (true)
     {
-		if( ! EStop->State() ) {
-			XMotor->MoveStopDecel(0);
+		if( ! EStop.State() ) {
+			XMotor.MoveStopDecel(0);
 			continue;
 		}
-		if( ! RightLimit->State() && ! LeftLimit->State() ) {
-			XMotor->MoveStopDecel(0);
+		if( ! RightLimit.State() && ! LeftLimit.State() ) {
+			XMotor.MoveStopDecel(0);
 			continue;
 		}
 		
-		if( ! LeftLimit->State() ) {
-			currentMove = MOVING_RIGHT;
+		if( ! LeftLimit.State() ) {
+			currentMove = MoveDirection::MOVING_RIGHT;
 		}
-		else if( ! RightLimit->State() ) {
-			currentMove = MOVING_LEFT;
+		else if( ! RightLimit.State() ) {
+			currentMove = MoveDirection::MOVING_LEFT;
 		}
 
 		switch(currentMove) {
-			case MOVING_RIGHT:
-				XMotor->MoveVelocity(velocity RIGHT);
+			case MoveDirection::MOVING_RIGHT:
+				XMotor.MoveVelocity(velocity RIGHT);
 				break;
 				
-			case MOVING_LEFT:
-				XMotor->MoveVelocity(velocity LEFT);
+			case MoveDirection::MOVING_LEFT:
+				XMotor.MoveVelocity(velocity LEFT);
 				break;
 				
 			default:
-				XMotor->MoveStopDecel(0);
+				XMotor.MoveStopDecel(0);
 		}
     }
 }
