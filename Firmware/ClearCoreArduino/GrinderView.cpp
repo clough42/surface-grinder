cbaa8520204a204446dc4d5fa093e20e3f15104a
clough42 <clough42@gmail.com>
2024-10-31 21:30:58 -0600

    Fix missing header.

--- Firmware/ClearCoreArduino/GrinderView.cpp
+++ Firmware/ClearCoreArduino/GrinderView.cpp
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#include <cstdint>
 #include "GrinderView.h"
 #include "HMIConstants.h"
 #include "IUserActions.h"
